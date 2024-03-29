b315655a5d985b2d21bcb880f918f1740a57f632
JuanPabloAM <jpam1888@gmail.com>
2024-03-29 16:00:22 -0500

    Quartz sync: Mar 29, 2024, 4:00 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,10 +27,11 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
+  ],
+  right: [
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
-  right: [],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
