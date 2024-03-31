30e1a20412e98cba5c68aef59a98a1fa30d602ea
JuanPabloAM <jpam1888@gmail.com>
2024-03-30 21:18:39 -0500

    Quartz sync: Mar 30, 2024, 9:18 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,8 +22,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Schibsted Grotesk",
+        header: "Lora",
+        body: "Lora",
         code: "Lora",
       },
       colors: {
